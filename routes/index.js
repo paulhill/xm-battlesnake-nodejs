4619c3232984b43b26183f55c3c943a66d4cce5b
Chrissy Simpson <csimpson@xmatters.com>
2017-06-08 14:33:20 -0700

    payload

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var router  = express.Router()
+var bodyParser = require('body-parser')
+var app = express()
+app.use(bodyParser.json())
 
 // Handle POST request to '/start'
 router.post('/start', function (req, res) {
@@ -19,11 +22,20 @@ router.post('/start', function (req, res) {
 // Handle POST request to '/move'
 router.post('/move', function (req, res) {
   // NOTE: Do something here to generate your move
-
+  var height = req.body.height
+  var width = req.body.width
+  var food = req.body.food
+  var snakes = req.body.snakes
+  var dead_snake = req.body.dead_snake
+  var you = req.body.you
+  console.log(req.body)
+  console.log('you=' + you)
+  console.log('height=' + height)
+  console.log('width=' + width)
   // Response data
   var data = {
-    move: 'up', // one of: ['up','down','left','right']
-    taunt: 'Outta my way, snake!', // optional, but encouraged!
+    move: 'down', // one of: ['up','down','left','right']
+    taunt: 'Outta my way, loser!', // optional, but encouraged!
   }
 
   return res.json(data)
